0b96f60ab9b41e054635280fce0ea405b9a3fd37
= <aamoos@naver.com>
2022-07-11 15:37:50 +0900

    Initial commit

---
+++ src/main/java/com/example/clothesrecommendation/ClothesRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothesrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clothesrecommendation/ClothesRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothesrecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

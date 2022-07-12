759e8232100011cbc93e5a9fc871e646dc21ab48
= <aamoos@naver.com>
2022-07-12 16:17:14 +0900

    vue.js 예제 추가

---
+++ src/main/java/com/example/clothesrecommendation/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.example.clothesrecommendation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String main(Model model){
+
+        return "main.html";
+    }
+
+}
